ec5ccb00e8657366708767d7d73b213fd9747b77
Dhawgupta <dhawal.cs15@iitp.ac.in>
2017-01-24 00:17:56 +0530

    defined feedfowrd

--- WEEK 2- Neural Network/challenge.py
+++ WEEK 2- Neural Network/challenge.py
@@ -9,14 +9,14 @@ class NeuralNetwork():
         random.seed(1)
 
         # making the framework of the model
-        for i in range(self.no_hl):
+        for i in range(self.no_hl + 1):
             if i == 0:
-                hidden_layer = 2*np.random.random((input_dim , no_neuron[i])) -1
-            elif i == self.no_hl -1:
-                hidden_layer = 2*np.random.random((no_of_neurons_per_layer[i],output_dim)) - 1
+                hidden_layer_weights = 2*np.random.random((input_dim , no_neuron[i])) -1
+            elif i == self.no_hl:
+                hidden_layer_weights = 2*np.random.random((no_of_neurons_per_layer[i],output_dim)) - 1
             else:
-                hidden_layer = 2*np.random.random((no_of_neurons_per_layer[i-1],no_of_neurons_per_layer[i]))
-            self.hidden_layers.append(hidden_layer)
+                hidden_layer_weights = 2*np.random.random((no_of_neurons_per_layer[i-1],no_of_neurons_per_layer[i]))
+            self.hidden_layers.append(hidden_layer_weights)
 
         def __sigmoid(self,x):
             # x is a tensor i.e. a matrix in this case
@@ -26,7 +26,24 @@ class NeuralNetwork():
         # in feedfowrd X is in format as [n,m] where n is the number of training samples and m is the no of features
 
         def feedforward(self,X):
+            cases = X.shape[0]
+            feature = X.shape[1]
+            output = []
             for i in range(self.no_hl + 1):
+                if i == 0:
+                    output = np.dot(X,self.hidden_layers[i])
+                    output = __sigmoid(output)
+                else:
+                    output = np.dot(output,hidden_layers[i])
+                    output = __sigmoid(output)
+            # now output will have a dimension of [m,output_dim]
+            return output
+
+        def train(self,X,y):
+            output = feedforward(X)
+            softmax_output = np.exp(output)/(np.sum(np.exp(output),axis = 1)).reshape(-1,1)
+            pred = np.argmax(softmax_output,axis = 1)
+
 
 # execution code
 """
